95854f71287d0096cbadae116548a8d386830f43
Nathaniel P. Marler <nmarler@sawgrassconsulting.com>
2013-02-22 15:08:56 -0600

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,13 +27,18 @@ describe 'Authentication' do
       before do
         fill_in 'Email', with: user.email.upcase
         fill_in 'Password', with: user.password
-        click_button 'Sign up'
+        click_button 'Sign in'
       end
 
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe 'followed by signout' do
+        before { click_link 'Sign out' }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
